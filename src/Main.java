8167160a2b0474b4960e167a7d66b9c6641b7a76
Aapo Pajunen <aapo.pajunen@gmail.com>
2017-09-25 15:01:12 +0300

    WOW! rectangulos

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
+import java.awt.geom.Rectangle2D;
+
+
 public class Main {
+
+    static Rectangle2D r1 = new Rectangle2D.Double(0,0,300,300);
 
     public static void main(String[] args) {
         System.out.println("Hello World!, noice!");
         System.out.println("kys");
         System.out.println("Tämä on uusi ominaisuus");
-                System.out.println("Noiiiiiicccc");
+        System.out.println("Noiiiiiicccc");
+        System.out.println(r1.toString());
+
     }
 
 
-}
+
+
+
+    }
+
+
+
