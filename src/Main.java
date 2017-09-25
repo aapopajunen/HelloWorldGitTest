a29212b54660e55ddd0935d6f2e42bb0f1bc43f5
Otto <otto.vonboehm@gmail.com>
2017-09-25 15:30:41 +0300

    kokeilu2.0

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!, noice!");
         System.out.println("kys");
+        System.out.println("jii");
+        System.out.println("AAMUJA");
     }
 
 
