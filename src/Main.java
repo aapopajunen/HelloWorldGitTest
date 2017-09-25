61f4a14e4cf8468dadb96e1c1e6809318f379ba7
Aapo Pajunen <aapo.pajunen@gmail.com>
2017-09-25 16:45:20 +0300

    new function added

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     static Rectangle2D r1 = new Rectangle2D.Double(0,0,300,300);
 
     public static void main(String[] args) {
-        System.out.println("Aapo")
+        System.out.println("Aapo");
+        System.out.println("new Print");
 
       
     }
